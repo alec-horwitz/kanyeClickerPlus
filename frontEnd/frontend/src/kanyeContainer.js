4de013f40212872405cb0e7aad742e07bcbfc3c5
zafinar <zafinar@gmail.com>
2018-05-04 13:53:28 -0400

    backend frontend merge and sync

--- frontEnd/frontend/src/kanyeContainer.js
+++ frontEnd/frontend/src/kanyeContainer.js
@@ -1,6 +1,15 @@
 import React from 'react'
 import NewUser from './Container/newUser.js'
 
+const API = "https://localhost:3000/api/v1/"
+
+const getRequest = (resource) => {
+  console.log(API+resource)
+  fetch(API+resource)
+  .then(r => r.json())
+  .then(json => this.setState({resource: json}))
+}
+
 class KanyeContainer extends React.Component{
 constructor(){
 super();
@@ -9,11 +18,16 @@ super();
   }
 }
 
+componentDidMount(){
+  getRequest("users")
+  getRequest("assets")
+  getRequest("scores")
+}
 
   render(){
     return(
       <div>
-      <NewUser/> 
+      <NewUser/>
       </div>
     )
   }
