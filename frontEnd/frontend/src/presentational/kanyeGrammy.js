896611f102184b36338e52959183b494a84412e6
zafinar <zafinar@gmail.com>
2018-05-07 17:48:21 -0400

    objectappearance

--- frontEnd/frontend/src/presentational/kanyeGrammy.js
+++ frontEnd/frontend/src/presentational/kanyeGrammy.js
@@ -1,18 +1,26 @@
 import React from 'react'
 
 const KanyeGrammy = (props) =>{
+  let i = 0
+  let grammyRender = []
+  let cost = 0
+  let imgURL = "./images/grammyBackdrop.png"
 
-    let cost = 0
       if (props.count < 1){
          cost = 5000
       }
       else {
          cost = 5000 * Math.floor(Math.pow(1.75,props.count))
       }
+      for (i; i<props.count; i++){
+        grammyRender.push(<img alt="" src="./images/grammy.jpg"/>)
+      }
       return(
         <div>
-        <img alt="" onClick={()=>props.purchase(cost,"grammy")} src="./images/grammyBackdrop.png"/>
-        {props.count}
+        <div style={{background: `url(${imgURL})`}}>
+        <button onClick={()=>props.purchase(cost,"grammy")}>{cost}</button>
+        {grammyRender}
+        </div>
         </div>
       )
 
