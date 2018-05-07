b99330d38d77dd9becd7c9f773b346aa1e2af70e
alec-horwitz <horwitz_alec@wheatoncollege.edu>
2018-05-07 17:57:14 -0400

    started new user page

--- backEnd/db/seeds.rb
+++ backEnd/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+i = 0
+while i < 10
+  i += 1
+  u = User.create(name:"#{i}", password:"#{i}")
+  a = Asset.create(user_id:i, yeezy:i, grammy:i, persianRug:i)
+  s = Score.create(user_id:i, value:i)
+end
